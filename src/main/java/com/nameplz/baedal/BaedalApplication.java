127328703319664c5a98828f3877d4d43677e906
barami62 <wodbs7758@naver.com>
2024-08-23 15:30:44 +0900

    first commit

---
+++ src/main/java/com/nameplz/baedal/BaedalApplication.java
@@ -0,0 +1,12 @@
+package com.nameplz.baedal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaedalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaedalApplication.class, args);
+    }
+}
