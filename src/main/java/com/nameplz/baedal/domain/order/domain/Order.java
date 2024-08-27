6da63b91f6769da4f6d14786ad9724ab7f182082
barami62 <wodbs7758@naver.com>
2024-08-27 14:24:52 +0900

    feat: 주문 관련 비즈니스 로직 추가
    - 주문 상태 변경
    - 주문 취소

--- src/main/java/com/nameplz/baedal/domain/order/domain/Order.java
+++ src/main/java/com/nameplz/baedal/domain/order/domain/Order.java
@@ -4,12 +4,15 @@ import com.nameplz.baedal.domain.model.Address;
 import com.nameplz.baedal.domain.model.BaseEntity;
 import com.nameplz.baedal.domain.store.domain.Store;
 import com.nameplz.baedal.domain.user.domain.User;
+import com.nameplz.baedal.global.common.exception.GlobalException;
+import com.nameplz.baedal.global.common.response.ResultCase;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.BatchSize;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -19,6 +22,9 @@ import java.util.UUID;
 @Table(name = "p_order")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order extends BaseEntity {
+
+    // 취소 가능한 시간 (분 단위) 변수명
+    private static final int ORDER_CANCELABLE_MINUTES = 5;
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -35,6 +41,7 @@ public class Order extends BaseEntity {
     @Column(name = "comment")
     private String comment;
 
+    @Column
     @Embedded
     private Address address;
 
@@ -64,7 +71,33 @@ public class Order extends BaseEntity {
         return order;
     }
 
+    /**
+     * 주문목록 추가
+     */
     public void addOrderLine(OrderLine orderLine) {
         orderLines.add(orderLine);
+    }
+
+    /**
+     * 주문 상태 변경 - 가게주인과 관리자만 가능
+     */
+    public void updateOrderStatus(OrderStatus orderStatus) {
+        // TODO : 권환 확인 필요
+        this.orderStatus = orderStatus;
+    }
+
+    /**
+     * 주문 취소 - 요구사항에 따라 5분 이내에만 취소 가능
+     */
+    public void cancelOrder() {
+        if (!isOrderCancelable()) {
+            throw new GlobalException(ResultCase.ORDER_CANCEL_DENIED);
+        }
+
+        this.orderStatus = OrderStatus.CANCELED;
+    }
+
+    private boolean isOrderCancelable() {
+        return LocalDateTime.now().isAfter(this.getCreatedAt().plusMinutes(ORDER_CANCELABLE_MINUTES));
     }
 }
