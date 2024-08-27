b502500c240d190cea56af4d224d993841b8f4f4
shurona <khc4669@naver.com>
2024-08-27 12:25:13 +0900

    feat: 불필요한 repository 어노테이션 삭제

--- src/main/java/com/nameplz/baedal/domain/territory/repository/TerritoryCustomRepositoryImpl.java
+++ src/main/java/com/nameplz/baedal/domain/territory/repository/TerritoryCustomRepositoryImpl.java
@@ -9,11 +9,9 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 @RequiredArgsConstructor
-@Repository
 public class TerritoryCustomRepositoryImpl implements TerritoryCustomRepository {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
