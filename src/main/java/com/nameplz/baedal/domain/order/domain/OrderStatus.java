f589614c5af53130e2a6d2573a2896b8f1bb1384
barami62 <wodbs7758@naver.com>
2024-08-28 10:50:54 +0900

    enhance: 필요없는 String -> ENUM 변환 로직 제거

--- src/main/java/com/nameplz/baedal/domain/order/domain/OrderStatus.java
+++ src/main/java/com/nameplz/baedal/domain/order/domain/OrderStatus.java
@@ -1,11 +1,7 @@
 package com.nameplz.baedal.domain.order.domain;
 
-import com.nameplz.baedal.global.common.exception.GlobalException;
-import com.nameplz.baedal.global.common.response.ResultCase;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor
@@ -18,12 +14,4 @@ public enum OrderStatus {
     CANCELED("주문 취소됨");
 
     private final String description;
-
-    public static OrderStatus findByName(String name) {
-
-        return Arrays.stream(OrderStatus.values())
-                .filter(orderStatus -> orderStatus.name().equals(name))
-                .findAny()
-                .orElseThrow(() -> new GlobalException(ResultCase.ORDER_STATUS_NOT_FOUND));
-    }
 }
--- src/main/java/com/nameplz/baedal/domain/order/domain/OrderType.java
+++ src/main/java/com/nameplz/baedal/domain/order/domain/OrderType.java
@@ -1,11 +1,7 @@
 package com.nameplz.baedal.domain.order.domain;
 
-import com.nameplz.baedal.global.common.exception.GlobalException;
-import com.nameplz.baedal.global.common.response.ResultCase;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor
@@ -15,12 +11,4 @@ public enum OrderType {
     OFFLINE("오프라인 주문");
 
     private final String description;
-
-    public static OrderType findByName(String name) {
-
-        return Arrays.stream(OrderType.values())
-                .filter(orderType -> orderType.name().equals(name))
-                .findAny()
-                .orElseThrow(() -> new GlobalException(ResultCase.ORDER_TYPE_NOT_FOUND));
-    }
 }
