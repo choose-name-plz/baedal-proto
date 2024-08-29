68c124ab27b1669257495cadf5377036fdbbfa4f
shurona <khc4669@naver.com>
2024-08-29 11:53:57 +0900

    fix: validatoin 메시지 추가

--- src/main/java/com/nameplz/baedal/domain/territory/dto/request/TerritoryUpdateRequestDto.java
+++ src/main/java/com/nameplz/baedal/domain/territory/dto/request/TerritoryUpdateRequestDto.java
@@ -3,7 +3,7 @@ package com.nameplz.baedal.domain.territory.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record TerritoryUpdateRequestDto(
-    @NotBlank
+    @NotBlank(message = "지역 이름을 입력해주세요")
     String name) {
 
 }
