7f6c4299010b4ff9150e683afc05e1ba5383ba0a
barami62 <wodbs7758@naver.com>
2024-08-31 17:03:32 +0900

    enhance: 주문목록 요청 DTO 스키마 추가

--- src/main/java/com/nameplz/baedal/domain/order/dto/request/CreateOrderLineRequestDto.java
+++ src/main/java/com/nameplz/baedal/domain/order/dto/request/CreateOrderLineRequestDto.java
@@ -1,10 +1,22 @@
 package com.nameplz.baedal.domain.order.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
+@Schema(description = "주문목록 생성 요청")
 public record CreateOrderLineRequestDto(
+
+        @Schema(description = "메뉴 ID", example = "123e4567-e89b-12d3-a456-426614174000")
         UUID menuId,
+
+        @Schema(description = "수량", example = "2")
+        @NotNull(message = "수량은 필수입니다.")
         Integer quantity,
+
+        @Schema(description = "가격", example = "10000")
+        @NotNull(message = "가격은 필수입니다.")
         Integer price
 ) {
 }
