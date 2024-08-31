c6c6fdf15110f58e9532544c03bbf454fd7470d1
barami62 <wodbs7758@naver.com>
2024-08-31 17:04:01 +0900

    enhance: 주문목록 응답 DTO 스키마 추가

--- src/main/java/com/nameplz/baedal/domain/order/dto/response/OrderLineResponseDto.java
+++ src/main/java/com/nameplz/baedal/domain/order/dto/response/OrderLineResponseDto.java
@@ -1,14 +1,26 @@
 package com.nameplz.baedal.domain.order.dto.response;
 
 import com.nameplz.baedal.domain.model.Money;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.UUID;
 
+@Schema(description = "주문목록 응답")
 public record OrderLineResponseDto(
+
+        @Schema(description = "주문 목록 ID", example = "123e4567-e89b-12d3-a456-426614174000")
         UUID orderLineId,
+
+        @Schema(description = "주문 ID", example = "123e4567-e89b-12d3-a456-426614174000")
         UUID orderId,
+
+        @Schema(description = "상품 ID", example = "123e4567-e89b-12d3-a456-426614174000")
         UUID productId,
+
+        @Schema(description = "상품 수량", example = "2")
         Integer quantity,
+
+        @Schema(description = "가격", example = "10000")
         Money amount
 ) {
 }
