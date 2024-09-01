755dcef42463325a82683614f8c47133ef24bbb0
barami62 <wodbs7758@naver.com>
2024-09-01 12:41:58 +0900

    enhance: 실제 결제에 필요한 요청 DTO로 수정

--- src/main/java/com/nameplz/baedal/domain/payment/dto/request/CreatePaymentRequestDto.java
+++ src/main/java/com/nameplz/baedal/domain/payment/dto/request/CreatePaymentRequestDto.java
@@ -1,12 +1,22 @@
 package com.nameplz.baedal.domain.payment.dto.request;
 
 import com.nameplz.baedal.domain.payment.domain.PaymentMethod;
-import com.nameplz.baedal.domain.payment.domain.PaymentStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Range;
 
 public record CreatePaymentRequestDto(
+
+        @Schema(description = "결제 금액", example = "10000")
+        @Range(message = "금액은 0 이상이어야 합니다.")
+        @NotNull(message = "금액은 필수입니다.")
         Integer amount,
-        String paymentKey,
+
+        @Schema(description = "결제 방식", example = "CARD")
         PaymentMethod method,
-        PaymentStatus status
+
+        @Schema(description = "결제자", example = "johndoe")
+        @NotNull(message = "결제자는 필수입니다.")
+        String username
 ) {
 }
