e64befd3985800e9102e0ae9ed6574ad11be1f8f
barami62 <wodbs7758@naver.com>
2024-09-01 14:43:07 +0900

    enhance: 인가처리를 클래스 레벨로 처리하여 중복 제거

--- src/main/java/com/nameplz/baedal/domain/payment/controller/PaymentController.java
+++ src/main/java/com/nameplz/baedal/domain/payment/controller/PaymentController.java
@@ -8,7 +8,6 @@ import com.nameplz.baedal.domain.user.domain.User;
 import com.nameplz.baedal.global.common.response.CommonResponse;
 import com.nameplz.baedal.global.common.response.EmptyResponseDto;
 import com.nameplz.baedal.global.common.security.annotation.IsMaster;
-import com.nameplz.baedal.global.common.security.annotation.IsMasterOrSelf;
 import com.nameplz.baedal.global.common.security.annotation.LoginUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,6 +25,7 @@ import java.util.UUID;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@IsMaster
 @Tag(name = "결제")
 @RequestMapping("/payments")
 public class PaymentController {
@@ -36,7 +36,6 @@ public class PaymentController {
      * 결제 조회
      */
     @GetMapping("/{paymentId}")
-    @IsMaster
     @Operation(summary = "결제 조회")
     public CommonResponse<PaymentResponseDto> getPayment(@PathVariable UUID paymentId) {
 
@@ -49,7 +48,6 @@ public class PaymentController {
      * 결제 리스트 조회
      */
     @GetMapping
-    @IsMasterOrSelf
     @Operation(summary = "결제 리스트 조회")
     public CommonResponse<List<PaymentResponseDto>> getPaymentList(
             @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
@@ -64,7 +62,6 @@ public class PaymentController {
      * 결제 처리
      */
     @PostMapping
-    @IsMaster
     @Operation(summary = "결제 처리")
     public CommonResponse<PaymentResponseDto> createPayment(@Valid @RequestBody CreatePaymentRequestDto request) {
 
@@ -77,7 +74,6 @@ public class PaymentController {
      * 결제 상태 변경
      */
     @PatchMapping("/{paymentId}/status")
-    @IsMaster
     @Operation(summary = "결제 상태 변경")
     public CommonResponse<EmptyResponseDto> updatePaymentStatus(
             @PathVariable UUID paymentId,
@@ -94,7 +90,6 @@ public class PaymentController {
      * 결제 삭제
      */
     @DeleteMapping("/{paymentId}")
-    @IsMaster
     @Operation(summary = "결제 삭제")
     public CommonResponse<EmptyResponseDto> deletePayment(
             @PathVariable UUID paymentId,
