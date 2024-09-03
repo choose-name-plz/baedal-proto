9e4e1b083eb282abcac0d07e46424d7e55d22f92
barami62 <wodbs7758@naver.com>
2024-09-04 01:18:29 +0900

    enhance: base package 설정
    - 표기를 안 하면 외부 패키지에서 사용 불가

--- src/main/java/com/nameplz/baedal/global/config/PropertyConfig.java
+++ src/main/java/com/nameplz/baedal/global/config/PropertyConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationPropertiesScan
+@ConfigurationPropertiesScan("com.nameplz.baedal")
 public class PropertyConfig {
 
 }
