ebd9abd33cec470f34b7638a47f24808b45dc4a7
barami62 <wodbs7758@naver.com>
2024-09-04 00:47:32 +0900

    feat: PropertyConfig 설정 객체 추가

---
+++ src/main/java/com/nameplz/baedal/global/config/PropertyConfig.java
@@ -0,0 +1,10 @@
+package com.nameplz.baedal.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan
+public class PropertyConfig {
+
+}
